b13641843aa0eb98272f2a5a346984cc3270352f
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2023-11-06 17:22:54 +0900

    Add files via upload

---
+++ Vue_Day06_Router/src/components/board/BoardCreate.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h4>게시판 등록입니다.</h4>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ Vue_Day06_Router/src/components/board/BoardList.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h4>게시판 목록입니다.</h4>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
